0d4f959ff95e7ace38d0e52a90fe5827c66a8151
Ezerskiy Igor <ezerskiyigor2000@gmail.com>
2023-01-30 15:19:39 +0200

    change mnozniki

--- HW_2lesson/mnozhnyky.py
+++ HW_2lesson/mnozhnyky.py
@@ -1,7 +1,15 @@
-k = int(input('Enter your number\n'))
+k = int(input('Enter your number: '))
 
-for i in range(1,k+1):
-    if k%i:
-        pass
-    else:
-        print(i)
+
+def odd(k_i: int) -> int:
+    result = []
+    for i in range(1, k_i+1):
+        if k % i:
+            pass
+        else:
+            result.append(i)
+    return result
+
+
+for i in odd(k):
+    print(i)
